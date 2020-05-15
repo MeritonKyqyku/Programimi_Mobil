1e4cb9ab93413f0c7dad6b65c70a800196e57877
MeritonKyqyku <43965461+MeritonKyqyku@users.noreply.github.com>
2020-05-16 00:45:56 +0200

    Registration pane

--- MyApplication/app/src/main/java/com/fiek/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/fiek/myapplication/MainActivity.java
@@ -3,13 +3,24 @@ package com.fiek.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public void clickToRegister(View view){
+
+
+        setContentView(R.layout.activity_registration);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
     }
+
+
+
 }
