a3f50086502dfcee61619bd7a026703a7b0d5657
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2020-05-21 21:35:52 +0200

    ndryshimet per navbar, shtimi i ic_menu, dhe ndryshime te ngjyrave te background

--- MyApplication/app/src/main/java/com/fiek/myapplication/Home.java
+++ MyApplication/app/src/main/java/com/fiek/myapplication/Home.java
@@ -1,56 +1,36 @@
 package com.fiek.myapplication;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-
 import com.google.android.material.navigation.NavigationView;
 
-public class Home extends MainActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Home extends AppCompatActivity {
 
-    ImageView navigationIcon;
-
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        drawerLayout = findViewById(R.id.layout_of_drawer);
-        navigationView = findViewById(R.id.nav_view);
+        final DrawerLayout drawerLayout = findViewById(R.id.layout_of_drawer);
 
-        navigationIcon = findViewById(R.id.navigation_icon);
-
-        navigationDrawer();
-    }
-
-    private void navigationDrawer() {
-
-        navigationView.bringToFront();
-        navigationView.setNavigationItemSelectedListener(this);
-        navigationView.setCheckedItem(R.id.nav_home);
-
-        navigationIcon.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.menu_icon).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(drawerLayout.isDrawerVisible(GravityCompat.START))
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                else drawerLayout.openDrawer(GravityCompat.START);
+                drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+
     }
 
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
-    }
+
 }
--- MyApplication/app/src/main/java/com/fiek/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/fiek/myapplication/MainActivity.java
@@ -1,28 +1,20 @@
 package com.fiek.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.View;
-import com.fiek.myapplication.RegistrationUser;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.EventListener;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
