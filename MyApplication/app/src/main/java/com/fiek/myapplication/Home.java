26475ead3f6cacf2acf375a99af454220ed7232e
MeritonKyqyku <43965461+MeritonKyqyku@users.noreply.github.com>
2020-05-30 00:10:22 +0200

    Inkorporimi i mapit

--- MyApplication/app/src/main/java/com/fiek/myapplication/Home.java
+++ MyApplication/app/src/main/java/com/fiek/myapplication/Home.java
@@ -1,21 +1,56 @@
 package com.fiek.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
 
+SupportMapFragment supportMapFragment;
+FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+       setContentView(R.layout.activity_home);
+
+
+        supportMapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+        client = LocationServices.getFusedLocationProviderClient(this);
+        if(ActivityCompat.checkSelfPermission(Home.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+            getCurrentLocation();
+        }
+        else {
+            ActivityCompat.requestPermissions(Home.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION},44
+            );
+        }
+
 
         final DrawerLayout drawerLayout = findViewById(R.id.layout_of_drawer);
 
@@ -23,14 +58,61 @@ public class Home extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(GravityCompat.START);
+                findViewById(R.id.nav_home).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getCurrentLocation();
+                    }
+                });
+
+
+            }
+        });
+        findViewById(R.id.addImageView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_add_image_view);
             }
         });
 
+
+
         NavigationView navigationView = findViewById(R.id.nav_view);
+
         navigationView.setItemIconTintList(null);
+
 
     }
 
 
+    private void getCurrentLocation() {
+        Task<Location> task= client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                if(location != null){
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback(){
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            LatLng latLng =new LatLng(location.getLatitude(),location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng).title("You are Here");
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                            googleMap.addMarker(options);
+                        }
 
+
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==44){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }
+        }
+    }
 }
---
+++ MyApplication/app/src/main/java/com/fiek/myapplication/addImageView.java
@@ -0,0 +1,6 @@
+package com.fiek.myapplication;
+
+import android.app.Activity;
+
+public class addImageView extends Activity {
+}
