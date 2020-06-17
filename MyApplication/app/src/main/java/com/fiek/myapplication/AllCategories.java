4d2706281dfdc5814a9a752de8b1b7e599dbf376
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2020-06-17 23:55:40 +0200

    Lidhja e navigation bar me fragmentet baze

---
+++ MyApplication/app/src/main/java/com/fiek/myapplication/AllCategories.java
@@ -0,0 +1,61 @@
+package com.fiek.myapplication;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AllCategories extends AppCompatActivity {
+
+    Animation topAnimation, bottomAnimation;
+    ImageView image;
+    TextView text;
+    ImageView backbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_categories);
+
+     // backbutton = findViewById(R.id.back_icon);
+
+     /* backbutton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              AllCategories.super.onBackPressed();
+          }
+      });*/
+
+        topAnimation= AnimationUtils.loadAnimation(this, R.anim.animation_top);
+        bottomAnimation= AnimationUtils.loadAnimation(this, R.anim.animation_bottom);
+
+        image = findViewById(R.id.images1);
+        text = findViewById(R.id.texts1);
+        image.setAnimation(topAnimation);
+        text.setAnimation(bottomAnimation);
+
+        image = findViewById(R.id.images2);
+        text = findViewById(R.id.texts2);
+        image.setAnimation(topAnimation);
+        text.setAnimation(bottomAnimation);
+
+        image = findViewById(R.id.images3);
+        text = findViewById(R.id.texts3);
+        image.setAnimation(topAnimation);
+        text.setAnimation(bottomAnimation);
+
+        image = findViewById(R.id.images4);
+        text = findViewById(R.id.texts4);
+        image.setAnimation(topAnimation);
+        text.setAnimation(bottomAnimation);
+
+        image = findViewById(R.id.images5);
+        text = findViewById(R.id.texts5);
+        image.setAnimation(topAnimation);
+        text.setAnimation(bottomAnimation);
+
+    }
+}
---
+++ MyApplication/app/src/main/java/com/fiek/myapplication/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.fiek.myapplication;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+LinearLayout locations;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        locations = v.findViewById(R.id.locations);
+
+
+        return v;
+
+
+    }
+
+
+
+}
