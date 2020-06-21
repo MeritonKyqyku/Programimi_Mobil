12757e360621f49f924f8f0e8a4f7c592e6469a5
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2020-06-21 18:21:39 +0200

    caudex font, dhe margin

--- MyApplication/app/src/main/java/com/fiek/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/fiek/myapplication/MainActivity.java
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
     public void clickToRegister(View view) {
         setContentView(R.layout.activity_registration);
